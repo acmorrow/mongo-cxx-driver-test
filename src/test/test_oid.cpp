5ecf606e951c5e9ad173127d6f4fb44002bb4f85
Andrew Morrow <acm@10gen.com>
2014-01-26 16:25:07 -0500

    Clean up test a bit

--- src/test/test_oid.cpp
+++ src/test/test_oid.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
-
 #include <gtest/gtest.h>
-
 #include <mongo/bson/bson.h>
-#include <mongo/client/dbclient.h>
 
 namespace {
 
-    // Check that we can reproduce the example from bsonspec.org
     TEST(OID, gen) {
-        const mongo::OID anOid = mongo::OID::gen();
+        const mongo::OID oid1 = mongo::OID::gen();
+        const mongo::OID oid2 = mongo::OID::gen();
+        EXPECT_NE(oid1, oid2);
     }
 
 } // namespace
